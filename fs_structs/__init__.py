795ffc0f21dbe6f1fd1988d24ce9df8b307f3a41
agboyano <agboyano@protonmail.com>
2026-02-25 18:12:38 +0100

    logging set to ERROR by default

--- fs_structs/__init__.py
+++ fs_structs/__init__.py
@@ -1,4 +1,8 @@
 __version__ = "0.0.1a"
 
 from . import structs
-from  . import watchdog
+from . import watchdog
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.getLogger(__name__).setLevel(logging.ERROR)
